b4fadcf6dce2c1b3287ebcfa1d5911c907ae1468
Andre MakaroFF <makarofff@ukr.net>
2018-02-01 00:15:50 +0200

    better code

--- lesson0/index.js
+++ lesson0/index.js
@@ -1,39 +1,32 @@
 let array = ['Oleg', 'Co', 'Ilia', 'Yan', 'Genya'];
 
-let allInOne = (array, number) => {
-  let filterArray = array.filter((name) => {
-    return name.length >= number;
-  });
+const allInOne = (array, number) => {
+    const filterArray = array.filter((name) => {
+        return name.length >= number;
+    });
 
-  let modifyArray = filterArray.map((name) => {
-    return {name: name, length: name.length}
-  });
+    const modifyArray = filterArray.map((name) => {
+        return {name: name, length: name.length}
+    });
 
-  return modifyArray;
+    return modifyArray;
 };
 
-let allInOneByReducer = (array, number) => {
-  let modifyArray = array.reduce((acc, item) => {
-    let obj = {name: item, length: item.length};
-
-    if(item.length >= number){
-      return acc.concat(obj);    //.push не работает
-    }else{
-      return acc;
-    }
-
-    // item.length >= number ? (acc.concat(obj)) : acc;  // не пойму почему не работает (через тернарный)
-
-  }, []);
-  return modifyArray;
+const allInOneByReducer = (array, number) => {
+    const modifyArray = array.reduce((acc, item) => {
+        const obj = {name: item, length: item.length};
+        item.length >= number ? acc.push(obj) : null;
+        return acc;
+    }, []);
+    return modifyArray;
 };
 
 let priceList = [{price: 12}, {price: 50}, {price: 13}];
-let sum = (priceList) => {
-  let total = priceList.reduce(function(a, b) {
-    return a + b.price;
-  }, 0);
-  return total;
+const sum = (priceList) => {
+    const total = priceList.reduce(function(a, b) {
+        return a + b.price;
+    }, 0);
+    return total;
 };
 
 console.log(allInOne(array,4));
